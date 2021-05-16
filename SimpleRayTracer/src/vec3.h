3456625ed07fa840a2bf1ced453d198965f333a4
unknown <lalo_pichu22@hotmail.com>
2021-05-16 17:10:38 -0500

    / operator fix

--- SimpleRayTracer/src/vec3.h
+++ SimpleRayTracer/src/vec3.h
@@ -79,7 +79,7 @@ inline vec3 operator*(const vec3& v, double t) {
 }
 
 inline vec3 operator/(vec3 v, double t) {
-  return (1 / t) * t;
+  return (1 / t) * v;
 }
 
 inline double dot(const vec3& u, const vec3& v) {
